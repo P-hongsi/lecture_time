4b065fb349637c2a8001405851f4e608cd4b165c
박홍시 <bakjy0312@gmail.com>
2024-07-01 17:48:22 +0900

    Add:만든 시간 디폴트값을 지정

--- src/var/models.py
+++ src/var/models.py
@@ -1,7 +1,7 @@
 """
 데이터베이스 테이블에 매핑될 모델 정의(ORM Model)
 """
-from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, Float, JSON, ForeignKey
+from sqlalchemy import Column, String, DateTime, func
 from var.session import Base
 
 class TimeTable(Base):
@@ -10,4 +10,4 @@ class TimeTable(Base):
     user_id = Column(String, primary_key=True)
     user_password = Column(String(128), nullable=False)  # null 값 허용안함
     data = Column(String, nullable=True)
-    create_date = Column(DateTime(timezone=True))
+    create_date = Column(DateTime(timezone=True), default=func.now(), nullable=False)
