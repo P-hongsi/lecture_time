3c3166be63108152a2a6a51894be5a27ff9ce0ce
박홍시 <bakjy0312@gmail.com>
2024-07-01 17:44:47 +0900

    Add:아이디 유효성 검사 추가

--- src/api/v1/login/login_dto.py
+++ src/api/v1/login/login_dto.py
@@ -32,6 +32,11 @@ class CreateUserInfo(BaseDTO):
         if not any(char.isalpha() for char in v):
             raise HTTPException(status_code=422, detail="비밀번호는 8자리 이상 영문과 숫자를 포함하여 작성해 주세요.")
         return v
+    @validator('user_id')
+    def validate_id(cls, v):
+        if len(v) < 8:
+            raise HTTPException(status_code=422, detail="아이디는 8자리 이상으로 작성해 주세요.")
+        return v
 
 class LoginForm(BaseDTO):
     username: Annotated[str, Field(description="유저 아이디")]
